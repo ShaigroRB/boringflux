719814462e99b3258e11d70a8e5ab61d4246f5d7
Shaigro <robin.boucher@epita.fr>
2025-12-06 00:11:49 +0100

    ui skeleton: add AppShell skeleton for future table of events

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,4 +1,12 @@
-import { Button, Container, Input, NumberInput, PasswordInput, Stack } from '@mantine/core'
+import {
+  AppShell,
+  Button,
+  Container,
+  Input,
+  NumberInput,
+  PasswordInput,
+  Stack
+} from '@mantine/core'
 import { useInputState } from '@mantine/hooks'
 
 function App(): React.JSX.Element {
@@ -10,19 +18,22 @@ function App(): React.JSX.Element {
     window.electron.ipcRenderer.send('rcon_connect', { host, port, password: pwd })
 
   return (
-    <>
-      <Container>
-        <Stack>
-          <h2>RCON connection</h2>
-          <Input.Wrapper label="Host">
-            <Input value={host} onChange={setHost} />
-          </Input.Wrapper>
-          <NumberInput label="Port" value={port} onChange={setPort} max={65535} min={0} />
-          <PasswordInput label="Password" value={pwd} onChange={setPwd} />
-          <Button onClick={rconConnect}>RCON should log in</Button>
-        </Stack>
-      </Container>
-    </>
+    <AppShell padding="md">
+      <AppShell.Navbar>
+        <Container>
+          <Stack>
+            <h2>RCON connection</h2>
+            <Input.Wrapper label="Host">
+              <Input value={host} onChange={setHost} />
+            </Input.Wrapper>
+            <NumberInput label="Port" value={port} onChange={setPort} max={65535} min={0} />
+            <PasswordInput label="Password" value={pwd} onChange={setPwd} />
+            <Button onClick={rconConnect}>Connect</Button>
+          </Stack>
+        </Container>
+      </AppShell.Navbar>
+      <AppShell.Main>{/* table of events */}</AppShell.Main>
+    </AppShell>
   )
 }
 
