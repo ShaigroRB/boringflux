4a7f65f7a70dc8ca0f09742e969bde02e2239a25
Shaigro <robin.boucher@epita.fr>
2025-12-05 23:59:20 +0100

    set mantineprovider

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,11 +1,15 @@
-import './assets/main.css'
+// import './assets/main.css'
+import '@mantine/core/styles.css'
 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import { MantineProvider } from '@mantine/core'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <MantineProvider defaultColorScheme="dark">
+      <App />
+    </MantineProvider>
   </StrictMode>
 )
