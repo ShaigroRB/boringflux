ad988b1535daca3e6823d0bdce98b33d1dba29ae
Shaigro <robin.boucher@epita.fr>
2025-12-22 00:09:28 +0100

    list events: refacto, move code into EventsTable component

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,5 +1,4 @@
 import {
-  Alert,
   AppShell,
   Button,
   Container,
@@ -9,30 +8,15 @@ import {
   Stack
 } from '@mantine/core'
 import { useInputState, useListState } from '@mantine/hooks'
-import { EmittedRconEvent, Packet } from '@shared/rcon'
-import { IconChevronDown, IconChevronUp, IconInfoCircle } from '@tabler/icons-react'
-import { DataTable } from 'mantine-datatable'
-import { useEffect, useState } from 'react'
-
-const IconInfo = <IconInfoCircle />
+import { EmittedRconEvent } from '@shared/rcon'
+import { useEffect } from 'react'
+import { EventsTable } from './EventsTable'
 
 function App(): React.JSX.Element {
   const [host, setHost] = useInputState('127.0.0.1')
   const [port, setPort] = useInputState<string | number>(42070)
   const [pwd, setPwd] = useInputState('admin')
   const [events, eventsHandlers] = useListState<{ EventID: string; Time: string; id: string }>([])
-  const [expandedRows, setExpandedRows] = useState<Record<string, boolean>>({})
-
-  const isConnected = events.length > 1
-
-  const isRowExpanded = (id: string) => expandedRows[id] ?? false
-
-  const toggleRowExpansion = (rowId: string) => {
-    const isExpanded = isRowExpanded(rowId)
-    setExpandedRows((current) => ({ ...current, [rowId]: !isExpanded }))
-  }
-
-  const rconConnect = (): void => window.api.rconConnect(host, Number(port), pwd)
 
   useEffect(() => {
     window.electron.ipcRenderer.on(EmittedRconEvent.NEW_RECEIVED_EVENT, (_, allEvents) => {
@@ -43,6 +27,10 @@ function App(): React.JSX.Element {
       window.electron.ipcRenderer.removeAllListeners(EmittedRconEvent.NEW_RECEIVED_EVENT)
     }
   })
+
+  const isConnected = events.length > 1
+
+  const rconConnect = (): void => window.api.rconConnect(host, Number(port), pwd)
 
   return (
     <AppShell padding="md" navbar={{ width: 300, breakpoint: 'xs' }}>
@@ -67,39 +55,7 @@ function App(): React.JSX.Element {
         </Container>
       </AppShell.Navbar>
       <AppShell.Main>
-        <Alert
-          w="fit-content"
-          variant="light"
-          color="blue"
-          title="Rows can be expanded to see all details of an event."
-          icon={IconInfo}
-        />
-        <DataTable
-          withColumnBorders
-          highlightOnHover
-          columns={[
-            {
-              accessor: 'id',
-              title: '#',
-              width: 50,
-              render: (record) =>
-                isRowExpanded(record.id) ? <IconChevronUp /> : <IconChevronDown />
-            },
-            {
-              accessor: 'EventID',
-              title: 'Event',
-              width: 200,
-              render: (record) => <>{Packet.Event[record.EventID]}</>
-            },
-            { accessor: 'Time' }
-          ]}
-          records={events}
-          onRowClick={({ record }) => toggleRowExpansion(record.id)}
-          rowExpansion={{
-            allowMultiple: true,
-            content: ({ record }) => <>{JSON.stringify(record, null, 2)}</>
-          }}
-        />
+        <EventsTable events={events} />
       </AppShell.Main>
     </AppShell>
   )
---
+++ src/renderer/src/EventsTable.tsx
@@ -0,0 +1,59 @@
+import { Alert } from '@mantine/core'
+
+import { IconChevronDown, IconChevronUp, IconInfoCircle } from '@tabler/icons-react'
+import { DataTable } from 'mantine-datatable'
+import { Packet } from '@shared/rcon'
+import { useState } from 'react'
+
+const IconInfo = <IconInfoCircle />
+
+export type EventsTableProps = {
+  events: { EventID: string; Time: string; id: string }[]
+}
+export const EventsTable = ({ events }: EventsTableProps) => {
+  const [expandedRows, setExpandedRows] = useState<Record<string, boolean>>({})
+
+  const isRowExpanded = (id: string) => expandedRows[id] ?? false
+
+  const toggleRowExpansion = (rowId: string) => {
+    const isExpanded = isRowExpanded(rowId)
+    setExpandedRows((current) => ({ ...current, [rowId]: !isExpanded }))
+  }
+
+  return (
+    <>
+      <Alert
+        w="fit-content"
+        variant="light"
+        color="blue"
+        title="Rows can be expanded to see all details of an event."
+        icon={IconInfo}
+      />
+      <DataTable
+        withColumnBorders
+        highlightOnHover
+        columns={[
+          {
+            accessor: 'id',
+            title: '#',
+            width: 50,
+            render: (record) => (isRowExpanded(record.id) ? <IconChevronUp /> : <IconChevronDown />)
+          },
+          {
+            accessor: 'EventID',
+            title: 'Event',
+            width: 200,
+            render: (record) => <>{Packet.Event[record.EventID]}</>
+          },
+          { accessor: 'Time' }
+        ]}
+        records={events}
+        onRowClick={({ record }) => toggleRowExpansion(record.id)}
+        rowExpansion={{
+          allowMultiple: true,
+          content: ({ record }) => <>{JSON.stringify(record, null, 2)}</>
+        }}
+      />
+    </>
+  )
+}
